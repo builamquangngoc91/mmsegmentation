71d451f099007cc358c46f39000d98aaf709ae8e
zach <bui.lam.quang.ngoc@moneyforward.vn>
2025-04-14 00:33:53 +0700

    change config size

--- configs/_base_/datasets/kpi.py
+++ configs/_base_/datasets/kpi.py
@@ -7,7 +7,7 @@ train_pipeline = [
     dict(type='LoadAnnotations', reduce_zero_label=True),
     dict(
         type='RandomResize',
-        scale=(512, 512),
+        scale=(2048, 2048),
         ratio_range=(1.0, 1.0),
         keep_ratio=True),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
@@ -17,7 +17,7 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', scale=(512, 512), keep_ratio=True),
+    dict(type='Resize', scale=(2048, 2048), keep_ratio=True),
     # add loading annotation after ``Resize`` because ground truth
     # does not need to do resize data transform
     dict(type='LoadAnnotations', reduce_zero_label=True),
